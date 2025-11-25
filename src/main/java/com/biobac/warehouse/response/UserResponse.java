781d3bf80963f33329f98cb468e9f130668af454
NarekSim <nsimonyan@curavel.com>
2025-11-25 21:53:50 +0400

    feat: set user in history record

---
+++ src/main/java/com/biobac/warehouse/response/UserResponse.java
@@ -0,0 +1,12 @@
+package com.biobac.warehouse.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserResponse {
+    private String firstname;
+    private String lastname;
+    private String username;
+}
